32c918e047e4a8032aab3a54ba64e7554c8ac5ce
Braeden Easterday <02braeden.easterday@gulllakecs.org>
2020-01-13 14:16:14 -0500

    Pulse Test

--- med_suit/med_suit.ino
+++ med_suit/med_suit.ino
@@ -6,11 +6,22 @@
  * Sensors will be added as I progress
  */
 
+int pulseSensor = 0;
+int led13 = 13;
+int Signal;
+int threshold = 550;
 
 void setup() {
-  
+  pinMode(led13, OUTPUT);
+  Serial.begin(9600);  
 }
 
 void loop() {
-  
+  Signal = analogRead(pulseSensor);
+  Serial.println(Signal);
+  if(Signal > threshold){
+    digitalWrite(led13,HIGH);
+  }else{
+    digitalWrite(led13,LOW);
+  }
 }
