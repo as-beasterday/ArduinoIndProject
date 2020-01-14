0f89692182709db0492af8e3a5b8feb76f7219ab
Braeden Easterday <02braeden.easterday@gulllakecs.org>
2020-01-14 14:06:39 -0500

    Fix Thermistor

--- med_suit/med_suit.ino
+++ med_suit/med_suit.ino
@@ -17,16 +17,12 @@ void setup() {
 }
 
 void loop() {
-  Vo = analogRead(ThermistorPin);
+  Vo = analogRead(thermistorPin);
   R2 = R1 * (1023.0 / (float)Vo - 1.0);
   logR2 = log(R2);
   T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
   T = T - 273.15;
   T = (T * 9.0)/ 5.0 + 32.0; 
-
-  Serial.print("Temperature: "); 
-  Serial.print(T);
-  Serial.println(" F"); 
 
   delay(500);
 }
